d5f28a87fd4b742c63e024fab78797cda0976854
Sultan00786 <treeloversultan0987@gmail.com>
2024-06-22 16:47:31 +0530

    feat: start to developed login and registor page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Group = lazy(() => import("./pages/Group"));
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/chat/:chatId" element={<Chat />} />
+          <Route path="/group" element={<Group />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
