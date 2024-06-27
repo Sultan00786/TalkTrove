ab64dd22f8404fa4cef5545a455f05fe30163e87
Sultan00786 <treeloversultan0987@gmail.com>
2024-06-27 17:07:02 +0530

    feat: Protected Route is added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { lazy } from "react";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
@@ -9,15 +10,25 @@ const Chat = lazy(() => import("./pages/Chat"));
 const Group = lazy(() => import("./pages/Group"));
 
 function App() {
+  const user = true; // to do
   return (
     <div className=" bg-sky-100 w-full h-[100vh]">
       <BrowserRouter>
         <Toaster position="top-right" reverseOrder={true} />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/chat/:chatId" element={<Chat />} />
-          <Route path="/group" element={<Group />} />
+          <Route element={<ProtectedRoute user={user} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat/:chatId" element={<Chat />} />
+            <Route path="/group" element={<Group />} />
+          </Route>
+          <Route
+            path="/login"
+            element={
+              <ProtectedRoute user={!user} redirect="/">
+                <Login />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+function ProtectedRoute({ children, user, redirect = "/login" }) {
+  if (!user) return <Navigate to={redirect} />;
+  else return children ? children : <Outlet />;
+}
+
+export default ProtectedRoute;
