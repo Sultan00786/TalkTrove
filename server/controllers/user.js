8760e59a72ea162fd666082197379a470f582f03
Sultan00786 <treeloversultan0987@gmail.com>
2024-07-02 11:27:30 +0530

    feat: start to create user api controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,23 @@
-const newUser = (req, res) => {
-  res.send("New user");
+import { User } from "../models/user.js";
+
+const newUser = async (req, res) => {
+  const avatar = {
+    public_id: "Sdfdfsaf",
+    url: "sdfss",
+  };
+
+  await User.create({
+    name: "Lakhan",
+    username: "Lakhan",
+    password: "Lakhan",
+    avatar,
+  });
+  console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+  res.status(201).json({
+    success: true,
+    message: "User Created Successfully",
+  });
 };
 
 const login = (req, res) => {
