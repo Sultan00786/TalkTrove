2c8b925dcea8d0541bb6a219dcfde9ec5442b521
Sultan00786 <treeloversultan0987@gmail.com>
2024-07-05 19:57:29 +0530

    feat: getMyChats and getMyGroup controller is created

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -13,8 +13,6 @@ const newGroup = TryCatch(async (req, res, next) => {
   }
 
   const allMembers = [...members, req.userId];
-
-  console.log(">>>>>>>>>>>>>>>>>" + req.userId);
   await Chat.create({
     name: name,
     groupChat: true,
@@ -31,4 +29,70 @@ const newGroup = TryCatch(async (req, res, next) => {
   });
 });
 
-export { newGroup };
+const getMyChats = TryCatch(async (req, res, next) => {
+  console.log(req.userId);
+  const chats = await Chat.find({ members: req.userId }).populate(
+    "members",
+    "name avatar"
+  );
+
+  if (!chats) {
+    return next(new ErrorHnadle("User chat not found", 404));
+  }
+
+  const transformChats = chats.map(({ _id, name, groupChat, members }) => {
+    return {
+      _id: _id,
+      groupChat: groupChat,
+      name: groupChat ? name : members[0].name,
+      avatar: !groupChat
+        ? [members[0].avatar.url]
+        : members.slice(0, 3).map((user) => user.avatar.url),
+      members: !groupChat
+        ? [members[0]._id]
+        : members
+            .filter((user) => user._id.toString() !== req.userId)
+            .map((user) => user._id),
+    };
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "All chats sends successfully ",
+    data: transformChats,
+  });
+});
+
+const getMyGroups = TryCatch(async (req, res, next) => {
+  console.log(req.userId);
+  const chats = await Chat.find({ members: req.userId }).populate(
+    "members",
+    "name avatar"
+  );
+
+  if (!chats) {
+    return next(new ErrorHnadle("User chat not found", 404));
+  }
+
+  const transformChats = chats.map(({ _id, name, groupChat, members }) => {
+    return {
+      _id: _id,
+      groupChat: groupChat,
+      name: name,
+      avatar: members.slice(0, 3).map((user) => user.avatar.url),
+      members: members.map((user) => user._id),
+    };
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "All chats sends successfully ",
+    data: transformChats,
+  });
+});
+
+const addMembers = TryCatch(async (req, res, next) => {
+  
+})
+
+export { newGroup, getMyChats, getMyGroups, addMembers};
