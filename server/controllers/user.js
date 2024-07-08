7c0cc54437ee1d43c5568190875446411fa88c5c
Sultan00786 <treeloversultan0987@gmail.com>
2024-07-08 10:38:52 +0530

    feat: getNotification and getMyFriend controller is created

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -6,6 +6,7 @@ import { ErrorHnadle } from "../utils/utility.js";
 import { Chat } from "../models/chat.js";
 import { Request } from "../models/request.js";
 import { NEW_REQUEST, REFETCH_CHATS } from "../constants/events.js";
+import { getOtherMember } from "../lib/helper.js";
 
 const cookieOptions = {
   maxAge: 15 * 24 * 60 * 60 * 1000,
@@ -127,7 +128,7 @@ const acceptFriendRequest = TryCatch(async (req, res, next) => {
     );
 
   if (!accept) {
-    await request.remove();
+    await Request.findByIdAndDelete(requestId);
     return res.status(200).json({
       success: true,
       message: "Friend Request Rejected!!!",
@@ -141,21 +142,83 @@ const acceptFriendRequest = TryCatch(async (req, res, next) => {
       members,
       name: `${request.sender.name}-${request.reciever.name}`,
     }),
-    request.remove(),
+    Request.findByIdAndDelete(requestId),
   ]);
 
-  emitEvent(
-    req,
-    REFETCH_CHATS,
-    members
-  )
+  emitEvent(req, REFETCH_CHATS, members);
 
   return res.status(200).json({
     success: true,
     message: "Friend Request Accepted!!!",
     senderId: request.sender._id,
-
   });
 });
 
-export { newUser, login, getMyProfile, logout, searchUser, sendFriendRequest, acceptFriendRequest };
+const getNotification = TryCatch(async (req, res, next) => {
+  const requests = await Request.find({ reciever: req.userId }).populate(
+    "sender",
+    "name avatar"
+  );
+  const data =
+    requests.map(({ _id, sender }) => ({
+      _id,
+      sender: {
+        _id: sender._id,
+        name: sender.name,
+        avatar: sender.avatar.url,
+      },
+    })) || [];
+
+  return res.status(200).json({
+    success: true,
+    message: "Get Notification Successful!!!",
+    data: data,
+  });
+});
+
+const getMyFriend = TryCatch(async (req, res, next) => {
+  const { chatId } = req.query;
+
+  const chats = await Chat.find({
+    groupChat: { $ne: true },
+    members: req.userId,
+  }).populate("members", "name avatar");
+
+  const friend = chats.map((chat) => {
+    const otherFriends = getOtherMember(chat.members, req.userId);
+    return {
+      _id: otherFriends._id,
+      name: otherFriends.name,
+      avatar: otherFriends.avatar.url,
+    };
+  });
+
+  if (chatId) {
+    const chat = await Chat.findById(chatId);
+    const avialableMembers = friend.filter(
+      (friend) => !chat.members.includes(friend._id)
+    );
+    return res.status(200).json({
+      success: true,
+      message: "Get My Friend Successful!!!",
+      data: avialableMembers,
+    });
+  } else {
+    return res.status(200).json({
+      success: true,
+      message: "Get My Friend Successful!!!",
+      data: friend,
+    });
+  }
+});
+export {
+  newUser,
+  login,
+  getMyProfile,
+  logout,
+  searchUser,
+  sendFriendRequest,
+  acceptFriendRequest,
+  getNotification,
+  getMyFriend,
+};
