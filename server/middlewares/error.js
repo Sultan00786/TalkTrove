33b659e8779a96a00713e6732a54306c6927ce48
Sultan00786 <treeloversultan0987@gmail.com>
2024-07-09 23:19:34 +0530

    fixs: in errorMiddleware

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -2,6 +2,20 @@ const errorMiddleware = (err, req, res, next) => {
   err.message ||= "Internal server error";
   err.statusCode ||= 500;
 
+  console.log(err.statusCode);
+
+  if (err.code === 11000) {
+    const error = Object.keys(err.keyPattern).join(", ");
+    err.message = `Error occur because of dublicate - ${error}`;
+    err.statusCode = 400;
+  }
+
+  if (err.name === "CastError") {
+    const errPath = err.path;
+    err.message = `Invalid ${errPath}`;
+    err.statusCode = 400;
+  }
+
   return res.status(err.statusCode).json({
     success: false,
     message: err.message,
