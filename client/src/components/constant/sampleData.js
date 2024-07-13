6412dc03d683a065b1afbbec25b54371126cfedd
Sultan00786 <treeloversultan0987@gmail.com>
2024-07-13 06:39:46 +0530

    const: sampleData create for rendering

---
+++ client/src/components/constant/sampleData.js
@@ -0,0 +1,16 @@
+export const sampleChats = [
+  {
+    avatar: ["https://avatars.githubusercontent.com/u/12584982"],
+    name: "John Doe",
+    _id: "1",
+    groupChat: false,
+    members: ["1", "2"],
+  },
+  {
+    avatar: ["https://avatars.githubusercontent.com/u/12584982"],
+    name: "John Boi",
+    _id: "2",
+    groupChat: false,
+    members: ["1", "2"],
+  },
+];
