98d5335f58c7bf1953e8ed6b930f1febc6e589e6
Sultan00786 <treeloversultan0987@gmail.com>
2024-07-13 12:05:18 +0530

    sample user data added for rendering of Profile commponents

--- client/src/components/constant/sampleData.js
+++ client/src/components/constant/sampleData.js
@@ -18,3 +18,14 @@ export const sampleChats = [
     members: ["1", "2"],
   },
 ];
+
+export const sampleUser = {
+  _id: "123",
+  name: "ohny Cav",
+  username: "ohnycav1",
+  avatar: [
+    "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1085.jpg",
+  ],
+  bio: "I am a full-stack developer. ",
+  createdAt: new Date("2024-07-03T16:38:38.105+00:00"),
+};
---
+++ client/src/lib/CalculateTimeSpam.js
@@ -0,0 +1,60 @@
+export const calculateTimeSpam = (date) => {
+  const today = new Date();
+  const createdAt = date;
+  let diffTimeInMinutes = 13685.005866666666;
+  // let diffTimeInMinutes = (today - createdAt) / (1000 * 60);
+  console.log(diffTimeInMinutes);
+
+  // year time spam = 365×24×60=525600
+  if (diffTimeInMinutes >= 525600) {
+    const yearInMinutes = 525600;
+    let yearCount = 0;
+    while (diffTimeInMinutes >= yearInMinutes) {
+      yearCount++;
+      diffTimeInMinutes -= yearInMinutes;
+    }
+    return `${yearCount === 1 ? "a year ago" : yearCount + " years ago"}`;
+  }
+  // month time spam = 30×24×60=43200
+  else if (diffTimeInMinutes >= 43200) {
+    const monthInMinutes = 43200;
+    let monthCount = 0;
+    while (diffTimeInMinutes >= monthInMinutes) {
+      monthCount++;
+      diffTimeInMinutes -= monthInMinutes;
+    }
+    return `${monthCount === 1 ? "a month ago" : monthCount + " months ago"}`;
+  }
+  // week time spam = 7×24×60=10080
+  else if (diffTimeInMinutes >= 10080) {
+    const weekInMinutes = 10080;
+    let weekCount = 0;
+    while (diffTimeInMinutes >= weekInMinutes) {
+      weekCount++;
+      diffTimeInMinutes -= weekInMinutes;
+    }
+    return `${weekCount === 1 ? "a week ago" : weekCount + " weeks ago"}`;
+  }
+  // day time sapam = 24*60 = 1440
+  else if (diffTimeInMinutes >= 1440) {
+    const dayInMinutes = 1440;
+    let dayCount = 0;
+    while (diffTimeInMinutes >= dayInMinutes) {
+      dayCount++;
+      diffTimeInMinutes -= dayInMinutes;
+    }
+    return `${dayCount === 1 ? "a day ago" : dayCount + " days ago"}`;
+  }
+  // hour time spam = 60
+  else if (diffTimeInMinutes >= 60) {
+    const hourInMinutes = 60;
+    let hourCount = 0;
+    while (diffTimeInMinutes >= hourInMinutes) {
+      hourCount++;
+      diffTimeInMinutes -= hourInMinutes;
+    }
+    return `${hourCount === 1 ? "an hour ago" : hourCount + " hours ago"}`;
+  } else {
+    console.log("Just Now");
+  }
+};
