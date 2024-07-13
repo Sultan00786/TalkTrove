ac33a96695adaff94a45d769745a8b89de7f7ef5
Sultan00786 <treeloversultan0987@gmail.com>
2024-07-13 06:37:35 +0530

    feat: create ChatList which show users chats

--- client/src/components/specific/ChatList.jsx
+++ client/src/components/specific/ChatList.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { colors, Stack } from "@mui/material";
+import React from "react";
+import ChatItem from "../shared/ChatItem";
 
-function ChatList() {
+function ChatList({
+  w = "100",
+  chats = [],
+  chatId,
+  onlineUsers = [],
+  newMessagesAlert = [
+    {
+      chatId: "",
+      count: 0,
+    },
+  ],
+  handleDeleteChat,
+}) {
   return (
-    <div>ChatList</div>
-  )
+    <Stack width={w} direction={"column"}>
+      {chats.map((data, index) => {
+        const { name, isGroupChat, avatar, members, _id } = data;
+        const newMessageAlert = newMessagesAlert.find((i) => i.chatId === _id);
+        const isOnline = members?.some((memberId) =>
+          onlineUsers.includes(memberId)
+        );
+
+        return (
+          <ChatItem
+            index={index}
+            newMessageAlert={newMessageAlert}
+            isOnline={isOnline}
+            avatar={avatar}
+            name={name}
+            _id={_id}
+            key={_id}
+            groupChat={isGroupChat}
+            isChatOpen={chatId === _id}
+            handleDeleteChat={handleDeleteChat}
+          />
+        );
+      })}
+    </Stack>
+  );
 }
 
-export default ChatList
+export default ChatList;
