17d55a3aa4436024dbb57d2867f4f4d9d278cdc3
Sultan00786 <treeloversultan0987@gmail.com>
2024-07-14 13:12:52 +0530

    fixs:restore Suspense mui commponent in Applayou

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -25,7 +25,43 @@ const AppLayout = () => (WrappedCommponent) => {
         <div className=" mt-[4rem]"></div>
 
         <div>
-          
+          <Suspense fallback={<Loader />}>
+            <Grid container height={"calc(100vh - 4rem)"}>
+              <Grid
+                item
+                sm={4}
+                md={3}
+                lg={3}
+                sx={{
+                  display: { xs: "none", sm: "block" },
+                }}
+                height={"100%"}
+              >
+                <ChatList
+                  chats={sampleChats}
+                  chatId={chatId}
+                  handleDeleteChat={handleDeleteChat}
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={8} lg={6} md={5} height={"100%"}>
+                <WrappedCommponent {...props} />
+              </Grid>
+
+              <Grid
+                item
+                md={4}
+                lg={3}
+                sx={{
+                  display: { xs: "none", md: "block" },
+                }}
+                height={"100%"}
+                className="bg-black bg-opacity-95 text-white"
+              >
+                <Profile user={sampleUser} />
+              </Grid>
+            </Grid>
+          </Suspense>
         </div>
       </div>
     );
