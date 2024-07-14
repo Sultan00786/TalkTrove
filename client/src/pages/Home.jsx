f894032e1aa01441ec47fa1fcd6bf0942d05f0f4
Sultan00786 <treeloversultan0987@gmail.com>
2024-07-15 03:25:13 +0530

    feat: Styling is done in Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 import AppLayout from "../components/layout/AppLayout";
-import { Typography } from "@mui/material";
 
 function Home() {
-  return (
-    <div className=" text-center pt-3 h-full">
-      <Typography variant="h4" className=" ">
-        Select a friend to chat
-      </Typography>
-    </div>
-  );
+  return <div className=" bg-gray-300 h-full">Home</div>;
 }
 
 export default AppLayout()(Home);
