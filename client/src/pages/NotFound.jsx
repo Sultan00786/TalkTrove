1eb074bb0afb48812db6cf8790112adaa2dad342
Sultan00786 <treeloversultan0987@gmail.com>
2024-08-13 15:44:22 +0530

    feat: NotFound component is to create

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import error404 from "../media/error/error404.png";
+import { useSelector } from "react-redux";
+import { Button } from "@mui/material";
+import { Login, Home } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
+  const { user } = useSelector((state) => state.user);
+  const navigate = useNavigate();
   return (
-    <div>NotFound</div>
-  )
+    <div className="flex justify-center mt-10 h-full">
+      <div className=" w-fit flex flex-col items-center">
+        <img src={error404} className="h-[300px]" />
+        <p className=" font-semibold text-xl mt-1">
+          THE PAGE YOU WERE LOOKING FOR DOESN'T EXIST
+        </p>
+        <p className=" font-semibold text-gray-700">
+          YOU MAY HAVE MISTYPED THE ADDRESS OF THE PAGE MAY HAVE MOVED
+        </p>
+        <div className="mt-5">
+          {user ? (
+            <Button
+              variant="contained"
+              endIcon={<Home />}
+              onClick={() => navigate("/")}
+            >
+              Go to Home
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              endIcon={<Login />}
+              onClick={() => navigate("/login")}
+            >
+              Lets do Login
+            </Button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
