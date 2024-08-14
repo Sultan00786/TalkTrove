3deee201b357f83a3b1cc772739412c3f75c40a3
Sultan00786 <treeloversultan0987@gmail.com>
2024-08-14 18:28:21 +0530

    fixs: avatar of sender and reciever

--- client/src/components/message/MessageBox.jsx
+++ client/src/components/message/MessageBox.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 function MessageBox({ message, userId }) {
   //   console.log(message);
-  const avatar =
-    "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1085.jpg";
+  const isEqualUser = message.sender._id === userId;
+  const avatar = message?.sender?.avatar?.url;
   return (
     <div
       className={` flex items-start gap-4  px-3 py-2 rounded-lg ${
-        message.sender._id === userId
+        isEqualUser
           ? "bg-gray-100 text-gray-950 self-end max-w-[55%]"
-          : "bg-gray-950 text-gray-100 max-w-[59%]"
+          : "bg-gray-950 text-gray-100 w-fit max-w-[59%]"
       }`}
     >
-      {message.sender !== userId && (
-        <div className="w-12 h-14">
-          <img src={avatar} alt="" className=" rounded-full" />
+      {!isEqualUser && (
+        <div>
+          <img src={avatar} alt="" className=" w-[35px] rounded-full" />
         </div>
       )}
-      {message.content}
+      <p className=" w-fit">{message.content}</p>
     </div>
   );
 }
