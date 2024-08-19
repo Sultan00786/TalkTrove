4f04cdb99a5e4a008e9400701c27e3917e5e30fc
Sultan00786 <treeloversultan0987@gmail.com>
2024-08-19 18:46:40 +0530

    feat: Slide component is used for adding transition in chatList

--- client/src/components/shared/ChatItem.jsx
+++ client/src/components/shared/ChatItem.jsx
@@ -1,8 +1,7 @@
-import { Stack, Typography } from "@mui/material";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Slide } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import AvatarCard from "./AvatarCard";
-import { useNavigate } from "react-router-dom";
 
 function ChatItem({
   avatar,
@@ -18,41 +17,54 @@ function ChatItem({
 }) {
   const navigate = useNavigate();
 
+  const [show, setShow] = useState(false);
+
   const handlerClickOnFriendChat = () => {
     console.log("clicked");
     setPerticularChatI(_id);
     navigate(`/chat/${_id}`);
   };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShow(true);
+    }, index * 80); // Delay of 1000ms (1 second) before the transition starts
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Link
-      className="w-full"
-      to={`/chat/${_id}`}
-      onContextMenu={(e) => handleDeleteChat(e, _id, groupChat)}
-    >
-      <div
-        className={`relativeS flex gap-3 px-3 py-3 cursor-pointer  ${
-          isChatOpen
-            ? " bg-black text-white hover:bg-gray-900"
-            : "hover:bg-gray-300"
-        }`}
-        onClick={handlerClickOnFriendChat}
+    <Slide style={{ width: `100%` }} in={show} direction="right" timeout={230}>
+      <Link
+        className="w-full"
+        to={`/chat/${_id}`}
+        onContextMenu={(e) => handleDeleteChat(e, _id, groupChat)}
       >
-        <AvatarCard avatar={avatar} />
-        <div className="flex items-center justify-between">
-          <div className="font-semibold text-lg pl-2">{name}</div>
-          {newMessageAlert && (
-            <div className=" text-xs font-semibold pr-2">
-              {newMessageAlert?.count} New Message
+        <div
+          className={`relativeS flex gap-3 px-3 py-3 cursor-pointer  ${
+            isChatOpen
+              ? " bg-black text-white hover:bg-gray-900"
+              : "hover:bg-gray-300"
+          }`}
+          onClick={handlerClickOnFriendChat}
+        >
+          <AvatarCard avatar={avatar} />
+          <div className="flex items-center justify-between">
+            <div className="font-semibold text-lg pl-2">{name}</div>
+            {newMessageAlert && (
+              <div className=" text-xs font-semibold pr-2">
+                {newMessageAlert?.count} New Message
+              </div>
+            )}
+          </div>
+          {isOnline && (
+            <div className="absolute right-2 top-0 h-full flex items-center ">
+              <div className="w-2 h-2 rounded-full bg-green-700"></div>
             </div>
           )}
         </div>
-        {isOnline && (
-          <div className="absolute right-2 top-0 h-full flex items-center ">
-            <div className="w-2 h-2 rounded-full bg-green-700"></div>
-          </div>
-        )}
-      </div>
-    </Link>
+      </Link>
+    </Slide>
   );
 }
 
--- client/src/components/specific/ChatList.jsx
+++ client/src/components/specific/ChatList.jsx
@@ -1,4 +1,3 @@
-import { colors, Stack } from "@mui/material";
 import React from "react";
 import ChatItem from "../shared/ChatItem";
 
@@ -17,7 +16,7 @@ function ChatList({
   setPerticularChatI,
 }) {
   return (
-    <div className="flex flex-col items-center overflow-y-scroll h-full w-full">
+    <div className="flex flex-col overflow-y-scroll h-full w-full">
       {chats.map((data, index) => {
         const { name, isGroupChat, avatar, members, _id } = data;
         const newMessageAlert = newMessagesAlert.find((i) => i.chatId === _id);
@@ -26,19 +25,21 @@ function ChatList({
         );
 
         return (
-          <ChatItem
-            index={index}
-            newMessageAlert={newMessageAlert}
-            isOnline={isOnline}
-            avatar={avatar}
-            name={name}
-            _id={_id}
-            key={_id}
-            groupChat={isGroupChat}
-            isChatOpen={chatId === _id}
-            handleDeleteChat={handleDeleteChat}
-            setPerticularChatI={setPerticularChatI}
-          />
+          <>
+            <ChatItem
+              newMessageAlert={newMessageAlert}
+              isOnline={isOnline}
+              avatar={avatar}
+              name={name}
+              index={index}
+              _id={_id}
+              key={_id}
+              groupChat={isGroupChat}
+              isChatOpen={chatId === _id}
+              handleDeleteChat={handleDeleteChat}
+              setPerticularChatI={setPerticularChatI}
+            />
+          </>
         );
       })}
     </div>
