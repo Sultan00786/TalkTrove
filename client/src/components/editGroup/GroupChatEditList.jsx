2d8c6dee63359a9f150a86427de8e5de4ec2bec7
Sultan00786 <treeloversultan0987@gmail.com>
2024-09-02 22:56:38 +0530

    feat: getGroupChat function use for fetching all group chats from server

--- client/src/components/editGroup/GroupChatEditList.jsx
+++ client/src/components/editGroup/GroupChatEditList.jsx
@@ -1,31 +1,45 @@
-import React, { useState } from "react";
-import { sampleGroupChat } from "../constant/sampleData";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  getChatDetails,
+  getGroupChat,
+} from "../../operation/apiController/chatApi";
 import AvatarCard from "../shared/AvatarCard";
-import { Link, useParams } from "react-router-dom";
-import AddMemberDialog from "./AddMemberDialog";
-import DeleteGroup from "./DeleteGroup";
 
 function GroupChatEditList() {
   const pram = useParams();
+  const navigate = useNavigate();
+
   const chatId = pram.chatId;
-  const groupChat = sampleGroupChat;
-  
+
+  const [groupChat, setGroupChat] = useState([]);
+
+  useEffect(() => {
+    const fetchGroupChat = async () => {
+      const data = await getGroupChat(navigate);
+      setGroupChat(data);
+    };
+    fetchGroupChat();
+  }, []);
+
   return (
     <div className=" h-full flex flex-col bg-lime-200">
       {/* <div className=" mt-2"></div> */}
       {groupChat.map((chat, index) => (
-        <Link
-          to={`/groups/group-edit/${chat._id}`}
+        <div
           key={index}
           className={`  flex items-center gap-3 px-3 py-3 cursor-pointer  ${
             chatId === chat._id ? "bg-gray-950 text-white" : "hover:bg-lime-300"
           } `}
+          onClick={(event) => {
+            event.preventDefault();
+            navigate(`/groups/group-edit/${chat._id}`);
+          }}
         >
           <AvatarCard avatar={chat.avatar} />
           <div className="font-semibold">{chat.name}</div>
-        </Link>
+        </div>
       ))}
-      
     </div>
   );
 }
